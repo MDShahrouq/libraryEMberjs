3bfaefbfb43bac7cdd283267947f803b1ef7edfc
mdshahrouq <mdshah1994@gmail.com>
2017-01-27 15:01:42 +0530

    Added UI and Firebase db

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,36 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+
+       firebase: {
+     apiKey: "AIzaSyDSlcSqcur4OUp15vSGTiQ8OMvbBEYchEE",
+    authDomain: "emberdb-66ac4.firebaseapp.com",
+    databaseURL: "https://emberdb-66ac4.firebaseio.com",
+    storageBucket: "emberdb-66ac4.appspot.com",
+    messagingSenderId: "1087788928306"
+    },
+
+
+// <script src="https://www.gstatic.com/firebasejs/3.6.7/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyDSlcSqcur4OUp15vSGTiQ8OMvbBEYchEE",
+//     authDomain: "emberdb-66ac4.firebaseapp.com",
+//     databaseURL: "https://emberdb-66ac4.firebaseio.com",
+//     storageBucket: "emberdb-66ac4.appspot.com",
+//     messagingSenderId: "1087788928306"
+//   };
+//   firebase.initializeApp(config);
+// </script>
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -25,10 +55,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+     ENV.APP.LOG_ACTIVE_GENERATION = true;
+     ENV.APP.LOG_TRANSITIONS = true;
+     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+     ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
